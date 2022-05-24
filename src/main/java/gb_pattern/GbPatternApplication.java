2376968fb98c86c0920fd32a44ce7704c84f57f1
MikhailSkakunov <82500910+MikhailSkakunov@users.noreply.github.com>
2022-05-24 12:26:11 +0300

    Initial commit

---
+++ src/main/java/gb_pattern/GbPatternApplication.java
@@ -0,0 +1,13 @@
+package gb_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class GbPatternApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(GbPatternApplication.class, args);
+//    }
+//
+//}
